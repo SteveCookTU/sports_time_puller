241d08e384a4047547adbb5ee0a95c75482652e2
SteveCookTU <steviecook210@gmail.com>
2024-09-05 09:26:37 -0400

    Temporarily remove NHL

--- src/components/common.rs
+++ src/components/common.rs
@@ -10,7 +10,6 @@ pub fn Common() -> impl IntoView {
             <div class="flex bg-gray-500 h-16 w-screen drop-shadow-xl justify-around items-center">
                 <a class="nav-button" href="/sports_time_puller/cfb">"CFB"</a>
                 <a class="nav-button" href="/sports_time_puller/mlb">"MLB"</a>
-                <a class="nav-button" href="/sports_time_puller/nhl">"NHL"</a>
                 <a class="nav-button" href="/sports_time_puller/soccer">"Soccer"</a>
             </div>
             <App />
